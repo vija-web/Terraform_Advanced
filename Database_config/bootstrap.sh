fe4d34beac01f204cbf0fadec905767d387da2e5
vijay <vijayanthony28667@gmail.com>
2026-04-23 14:06:58 +0530

    added

--- Database_config/bootstrap.sh
+++ Database_config/bootstrap.sh
@@ -4,4 +4,4 @@ sudo dnf install ansible -y
 sudo mkdir -p /var/log/roboshop
 sudo touch /var/log/roboshop/ansible.log
 sudo chmod 666 /var/log/roboshop/ansible.log
-sudo ansible-pull -U https://github.com/vija-web/ansible_for_tf.git -e "component=mongodb" main.yml
+sudo ansible-pull -U https://github.com/vija-web/ansible_for_tf.git -C main -e "component=mongodb" main.yml
