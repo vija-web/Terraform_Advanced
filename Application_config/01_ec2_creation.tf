dd08901e9adcd8220aeb275a6a17d09a792c87dd
vijay <vijayanthony28667@gmail.com>
2026-04-26 17:33:47 +0530

    modified

--- Application_config/01_ec2_creation.tf
+++ Application_config/01_ec2_creation.tf
@@ -3,9 +3,8 @@ resource "aws_security_group" "sg" {
   egress {
     from_port        = 0
     to_port          = 0
-    protocol         = "-1"
+    protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 }
 
