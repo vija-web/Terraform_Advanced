59c00418a46b9f6523956c0b7a1504483afc4ffd
vijay <vijayanthony28667@gmail.com>
2026-04-26 17:56:53 +0530

    modified

--- Application_config/bootstrap.sh
+++ Application_config/bootstrap.sh
@@ -7,4 +7,4 @@ sudo dnf install ansible -y
 sudo mkdir -p /var/log/roboshop
 sudo touch /var/log/roboshop/ansible.log
 sudo chmod 666 /var/log/roboshop/ansible.log
-sudo ansible-pull -U https://github.com/vija-web/ansible_for_tf.git -C main -e "component=$COMPONENT" -e "project=$PROJECT" -e "env=$ENV"main.yaml
+sudo ansible-pull -U https://github.com/vija-web/ansible_for_tf.git -C main -e "component=$COMPONENT" -e "project=$PROJECT" -e "env=$ENV" main.yaml
