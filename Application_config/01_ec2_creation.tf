e23afcfbdeb4ed7c1c68f0c64d66aa08811b55c1
vijay <vijayanthony28667@gmail.com>
2026-04-27 08:01:22 +0530

    added the files

--- Application_config/01_ec2_creation.tf
+++ Application_config/01_ec2_creation.tf
@@ -1,12 +1,13 @@
 
 resource "aws_instance" "Catalogue" {
+  count = 2
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t3.micro"
   subnet_id = data.aws_ssm_parameter.application_subnet_ids[0].value
   vpc_security_group_ids = [ data.aws_ssm_parameter.Catalogue_sg_id.value ]
 
   tags = {
-    Name = "Catalogue"
+    Name = "Catalogue-${var.zones[count.index]}"
   }
   
   connection {
@@ -25,27 +26,28 @@ resource "aws_instance" "Catalogue" {
     inline = [
       "sleep 60",
       "chmod 777 /tmp/bootstrap.sh", 
-      "sudo sh /tmp/bootstrap.sh catalogue ${var.project} ${var.environment} ${var.zones[0]}"
+      "sudo sh /tmp/bootstrap.sh catalogue ${var.project} ${var.environment} ${var.zones[count.index]}"
     ]
   }
 }
 
 resource "aws_ami_from_instance" "catalogue_ami" {
+  count = 2
   name               = "catalogue-custom-ami"
-  source_instance_id = aws_instance.Catalogue.id
+  source_instance_id = aws_instance.Catalogue[count.index].id
 
   depends_on = [
     aws_instance.Catalogue
   ]
 
   tags = {
-    Name = "catalogue-ami"
+    Name = "catalogue-ami-${var.zones[count.index]}"
   }
 }
 
 resource "aws_instance" "Catalogue_latest" {
   count = 2
-  ami           = aws_ami_from_instance.catalogue_ami.id
+  ami           = aws_ami_from_instance.catalogue_ami[count.index].id
   instance_type = "t3.micro"
   subnet_id = data.aws_ssm_parameter.application_subnet_ids[count.index].value
   vpc_security_group_ids = [ data.aws_ssm_parameter.Catalogue_sg_id.value ]
