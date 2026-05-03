f44615489db65cd901d4222ffb4507215d61045c
vijay <vijayanthony28667@gmail.com>
2026-05-04 00:02:45 +0530

    renamed

--- Application_Module/ssmparameters.tf
+++ Application_Module/ssmparameters.tf
@@ -8,5 +8,5 @@ resource "aws_ssm_parameter" "ami_ids" {
 resource "aws_ssm_parameter" "target_group_arn" {
   type  = "String"
   name  = "${var.component}-tg-arn"
-  value = "aws_lb_target_group.application_tg.arn"
+  value = aws_lb_target_group.application_tg.arn
 }
